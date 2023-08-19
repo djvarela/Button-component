88c581db7ae4d73e4d1cdee58d525b4eeb9400fa
Diego J. Varela <Djvarela89@gmail.com>
2023-08-19 12:16:08 -0300

    test

--- buttons/src/App.jsx
+++ buttons/src/App.jsx
@@ -1,12 +1,15 @@
 import "./normalize.css";
 import "./App.css";
 import { Main, Footer } from "./components";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Main/>
       <Footer />
+    </BrowserRouter>
     </>
   );
 }
--- buttons/src/main.jsx
+++ buttons/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
@@ -8,9 +8,9 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <BrowserRouter>
+  
       <App/>
-    </BrowserRouter>
+
     
   </React.StrictMode>,
 )
